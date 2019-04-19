c7e49c2010b19712a55dc701063c3c8cc98c2f67
annaq2001 <29985594+annaq2001@users.noreply.github.com>
2019-04-19 14:42:04 -0700

    testing

--- Assets/Food Fight/Scripts/test.cs
+++ Assets/Food Fight/Scripts/test.cs
@@ -4,13 +4,27 @@ using UnityEngine;
 
 public class test : MonoBehaviour {
 
+    private Rigidbody rb;
+
 	// Use this for initialization
 	void Start () {
-		
-	}
+		rb = GetComponent<Rigidbody>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    void OnCollisionEnter(Collision obj)
+    {
+        Debug.Log(obj.gameObject.name);
+        if (obj.gameObject.name == "Plane")
+        {
+            Destroy(obj.gameObject);
+        }
+        else
+            rb.AddForce(transform.up * 100, ForceMode.Force);
+    }
 }
+
